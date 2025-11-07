69766c0dd66c66ef91f1190469dd7962b7c34d43
Eric Sandhu <sandhue098@gmail.com>
2025-11-08 00:17:39 +0530

    Fix formatting in rowAndMaximumOnes function

--- 26.cpp
+++ 26.cpp
@@ -7,7 +7,7 @@ public:
         int maxCount = 0;      
         int rowIndex = 0;      
         
-        for (int i = 0; i < mat.size(); i++) {
+        for (int i = 0; i < mat.size(); i++) {   
             int count = 0;
             for (int j = 0; j < mat[i].size(); j++) {
                 if (mat[i][j] == 1) count++;
